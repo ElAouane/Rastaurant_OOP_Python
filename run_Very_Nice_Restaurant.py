47d116bfa899faecabeae24ee79bc15ee699bb88
Hamza El Aouane <haouane@spartaglobal.com>
2020-02-11 11:54:25 +0000

    Created People and Customers Class

---
+++ run_Very_Nice_Restaurant.py
@@ -0,0 +1,8 @@
+from People_class import People, Customer
+
+# As a restaurant owner i can create new customers
+customer1 = Customer('John', '21 down town Abi, London')
+customer2 = Customer('Anabela', 'Hackney, London')
+
+
+#as a restaurant owner i can create new orders with food item for a customer
